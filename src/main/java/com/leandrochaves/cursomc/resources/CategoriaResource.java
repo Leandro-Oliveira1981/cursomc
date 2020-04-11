23fdb38e64e61b8fbc65b8c68c9386be749678ce
Leandro Oliveira <leandro.systemas@gmail.com>
2020-04-11 18:05:39 -0300

    validacao sintatica com Bean validation

--- src/main/java/com/leandrochaves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/leandrochaves/cursomc/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.Servlet;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -38,7 +39,8 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj){		
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){	
+		Categoria obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.
 				  fromCurrentRequest().path("/id{}").
@@ -47,7 +49,8 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
+		Categoria obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		
--- src/main/java/com/leandrochaves/cursomc/services/CategoriaService.java
+++ src/main/java/com/leandrochaves/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leandrochaves.cursomc.domain.Categoria;
+import com.leandrochaves.cursomc.dto.CategoriaDTO;
 import com.leandrochaves.cursomc.repositories.CategoriaRepository;
 import com.leandrochaves.cursomc.services.exception.DataIntegrityException;
 import com.leandrochaves.cursomc.services.exception.ObjectNotFoundException;
@@ -57,5 +58,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());
 	}
 }
